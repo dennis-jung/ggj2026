6c879afa58ff8f5b8452a8a7bbde379c6e53e4a7
Florian Wuensche <florian.wuensche@live.com>
2026-01-30 18:14:50 -0500

    Test SpriteMesh

--- ballroom.gd
+++ ballroom.gd
@@ -1,37 +1,71 @@
 extends Node3D
 class_name Ballroom
 
+@export var transparency_zone : float = 15.0
+
 @onready var wallEast : MeshInstance3D = $Wall00
 @onready var wallNorth : MeshInstance3D = $Wall01
 @onready var wallWest : MeshInstance3D = $Wall02
 @onready var wallSouth : MeshInstance3D = $Wall03
 
 func set_wall_visibility(rotation_y : float) -> void:
-	print(rotation_y)
 	var clamped_rotation_deg : int  = int(rad_to_deg(rotation_y)) % 360
 	if (clamped_rotation_deg < 0):
 		clamped_rotation_deg = clamped_rotation_deg + 360
-	if (clamped_rotation_deg > 0 and clamped_rotation_deg <= 90):
-		print("SE", clamped_rotation_deg)
-		wallSouth.hide()
-		wallEast.hide()
-		wallNorth.show()
-		wallWest.show()
-	elif (clamped_rotation_deg > 90 and clamped_rotation_deg <= 180):
-		print("NE", clamped_rotation_deg)
-		wallSouth.show()
-		wallEast.hide()
-		wallNorth.hide()
-		wallWest.show()
-	elif (clamped_rotation_deg > 180 and clamped_rotation_deg <= 270):
-		print("NW", clamped_rotation_deg)
-		wallSouth.show()
-		wallEast.show()
-		wallNorth.hide()
-		wallWest.hide()
-	elif (clamped_rotation_deg > 270 and clamped_rotation_deg <= 360):
-		print("SW", clamped_rotation_deg)
-		wallSouth.hide()
-		wallEast.show()
-		wallNorth.show()
-		wallWest.hide()
+		
+	# south wall
+	if (clamped_rotation_deg >= 135.0 - transparency_zone and clamped_rotation_deg <= 225.0 + transparency_zone):
+		wallSouth.transparency = 0.0
+		#print("transparency = ", 0.0, " @ ", clamped_rotation_deg, " deg")
+	elif (clamped_rotation_deg <= 45.0 + transparency_zone or clamped_rotation_deg >= 315.0 - transparency_zone):
+		wallSouth.transparency = 1.0
+		#print("transparency = ", 1.0, " @ ", clamped_rotation_deg, " deg")
+	elif (clamped_rotation_deg < 135.0):
+		wallSouth.transparency = calculate_transparency(clamped_rotation_deg, 135.0 - transparency_zone, 45.0 + transparency_zone)
+		print("south wall transparency: ", wallSouth.transparency)
+	else:
+		wallSouth.transparency = calculate_transparency(clamped_rotation_deg, 225.0 + transparency_zone, 315.0 - transparency_zone)
+		print("south wall transparency: ", wallSouth.transparency)
+	
+	# east wall
+	if (clamped_rotation_deg >= 225.0 - transparency_zone and clamped_rotation_deg <= 315.0 + transparency_zone):
+		wallEast.transparency = 0.0
+		#print("transparency = ", 0.0, " @ ", clamped_rotation_deg, " deg")
+	elif (clamped_rotation_deg <= 135.0 + transparency_zone):
+		wallEast.transparency = 1.0
+		#print("transparency = ", 1.0, " @ ", clamped_rotation_deg, " deg")
+	else:
+		wallEast.transparency = calculate_transparency(clamped_rotation_deg, 225.0 - transparency_zone, 135 + transparency_zone)
+		print("east wall transparency: ", wallEast.transparency)
+
+	# north wall
+	if (clamped_rotation_deg >= 135.0 - transparency_zone and clamped_rotation_deg <= 225.0 + transparency_zone):
+		wallNorth.transparency = 1.0
+		#print("transparency = ", 0.0, " @ ", clamped_rotation_deg, " deg")
+	elif (clamped_rotation_deg <= 45.0 + transparency_zone or clamped_rotation_deg >= 315.0 - transparency_zone):
+		wallNorth.transparency = 0.0
+		#print("transparency = ", 1.0, " @ ", clamped_rotation_deg, " deg")
+	elif (clamped_rotation_deg < 135.0):
+		wallNorth.transparency = calculate_transparency(clamped_rotation_deg, 45.0 + transparency_zone, 135.0 - transparency_zone)
+		print("north wall transparency: ", wallNorth.transparency)
+	else:
+		wallNorth.transparency = calculate_transparency(clamped_rotation_deg, 315.0 - transparency_zone, 225.0 + transparency_zone)
+		print("north wall transparency: ", wallNorth.transparency)
+	
+	# west wall
+	if (clamped_rotation_deg >= 225.0 - transparency_zone and clamped_rotation_deg <= 315.0 + transparency_zone):
+		wallWest.transparency = 1.0
+		#print("transparency = ", 0.0, " @ ", clamped_rotation_deg, " deg")
+	elif (clamped_rotation_deg <= 135.0 + transparency_zone):
+		wallWest.transparency = 0.0
+		#print("transparency = ", 1.0, " @ ", clamped_rotation_deg, " deg")
+	else:
+		wallWest.transparency = calculate_transparency(clamped_rotation_deg, 135 + transparency_zone, 225.0 - transparency_zone)
+		print("west wall transparency: ", wallWest.transparency)
+
+
+func calculate_transparency(current : float, from : float, to : float) -> float:
+	var delta : float = abs(to - from)
+	var ratio : float = abs ((current - from) / delta)
+	return ratio
+	
--- main.gd
+++ main.gd
@@ -3,30 +3,41 @@ extends Node3D
 @onready var ballroom : Ballroom = $Ballroom
 @onready var camera_pivot : Node3D = $CameraPivot
 @onready var camera : Camera3D = $CameraPivot/Camera3D
+@onready var cameraTarget : Node3D = $CameraPivot/CameraTarget
 
 var rotation_tween : Tween = null
 var current_rotation_target : float = 0.0
+var is_rotating : bool = false
 
 func _ready() -> void:
-	camera.look_at(Vector3.ZERO)
+	camera.look_at(cameraTarget.position)
 	camera_pivot.rotation.y = deg_to_rad(45.0)
 	current_rotation_target = camera_pivot.rotation.y
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_left"):
+	if event.is_action_pressed("rotate_left"):
 		current_rotation_target = current_rotation_target + deg_to_rad(90.0)
 		rotate_camera()
-	if event.is_action_pressed("ui_right"):
+	if event.is_action_pressed("rotate_right"):
 		current_rotation_target = current_rotation_target - deg_to_rad(90.0)
 		rotate_camera()
+	if event.is_action("zoom_in"):
+		camera.translate_object_local(Vector3(0.0, 0.0, -1.0))
+	if event.is_action("zoom_out"):
+		camera.translate_object_local(Vector3(0.0, 0.0, +1.0))
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	ballroom.set_wall_visibility(camera_pivot.rotation.y)
 
 func rotate_camera() -> void:
+	is_rotating = true
 	if rotation_tween:
 		rotation_tween.kill()
 	rotation_tween = create_tween()
+	rotation_tween.set_trans(Tween.TRANS_CUBIC)
 	var target_rotation = Vector3(0.0, current_rotation_target, 0.0)
 	rotation_tween.tween_property(camera_pivot, "rotation", target_rotation, 1.0)
-	
+	rotation_tween.tween_callback(end_rotation)
+
+func end_rotation() -> void:
+	is_rotating = false
