51ea3d2827811af882406b728d9b12630c8b5c28
Florian Wuensche <florian.wuensche@live.com>
2026-01-30 15:45:22 -0500

    Rotieren mit tweens, Walls ausblenden anhand Kamerawinkel

--- ballroom.gd
+++ ballroom.gd
@@ -6,26 +6,32 @@ class_name Ballroom
 @onready var wallWest : MeshInstance3D = $Wall02
 @onready var wallSouth : MeshInstance3D = $Wall03
 
-func set_wall_visibility(direction):
-	match direction:
-		"southeast":
-			wallSouth.hide()
-			wallEast.hide()
-			wallNorth.show()
-			wallWest.show()
-		"northeast":
-			wallSouth.show()
-			wallEast.hide()
-			wallNorth.hide()
-			wallWest.show()
-		"northwest":
-			wallSouth.show()
-			wallEast.show()
-			wallNorth.hide()
-			wallWest.hide()
-		"southwest":
-			wallSouth.hide()
-			wallEast.show()
-			wallNorth.show()
-			wallWest.hide()
-	#print("Wall visibility: %s/%s/%s/%s", wallSouth.vivible, wallEast.visible, wallNorth.visible, wallWest.visible)
+func set_wall_visibility(rotation_y : float) -> void:
+	print(rotation_y)
+	var clamped_rotation_deg : int  = int(rad_to_deg(rotation_y)) % 360
+	if (clamped_rotation_deg < 0):
+		clamped_rotation_deg = clamped_rotation_deg + 360
+	if (clamped_rotation_deg > 0 and clamped_rotation_deg <= 90):
+		print("SE", clamped_rotation_deg)
+		wallSouth.hide()
+		wallEast.hide()
+		wallNorth.show()
+		wallWest.show()
+	elif (clamped_rotation_deg > 90 and clamped_rotation_deg <= 180):
+		print("NE", clamped_rotation_deg)
+		wallSouth.show()
+		wallEast.hide()
+		wallNorth.hide()
+		wallWest.show()
+	elif (clamped_rotation_deg > 180 and clamped_rotation_deg <= 270):
+		print("NW", clamped_rotation_deg)
+		wallSouth.show()
+		wallEast.show()
+		wallNorth.hide()
+		wallWest.hide()
+	elif (clamped_rotation_deg > 270 and clamped_rotation_deg <= 360):
+		print("SW", clamped_rotation_deg)
+		wallSouth.hide()
+		wallEast.show()
+		wallNorth.show()
+		wallWest.hide()
--- main.gd
+++ main.gd
@@ -1,40 +1,32 @@
 extends Node3D
 
 @onready var ballroom : Ballroom = $Ballroom
-@onready var camera : Camera3D = $Camera3D
-var view_point = 0
+@onready var camera_pivot : Node3D = $CameraPivot
+@onready var camera : Camera3D = $CameraPivot/Camera3D
+
+var rotation_tween : Tween = null
+var current_rotation_target : float = 0.0
 
 func _ready() -> void:
-	set_viewpoint()
+	camera.look_at(Vector3.ZERO)
+	camera_pivot.rotation.y = deg_to_rad(45.0)
+	current_rotation_target = camera_pivot.rotation.y
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_left"):
-		view_point = view_point + 1
-		if view_point > 3:
-			view_point = 0
-		set_viewpoint()
+		current_rotation_target = current_rotation_target + deg_to_rad(90.0)
+		rotate_camera()
 	if event.is_action_pressed("ui_right"):
-		view_point = view_point - 1
-		if view_point < 0:
-			view_point = 3
-		set_viewpoint()
+		current_rotation_target = current_rotation_target - deg_to_rad(90.0)
+		rotate_camera()
 
-func set_viewpoint() -> void:
-	match view_point:
-		0:
-			ballroom.set_wall_visibility("southeast")
-			camera.position = Vector3(25.0, 25.0, 25.0)
-			camera.look_at(Vector3.ZERO)
-		1:
-			ballroom.set_wall_visibility("northeast")
-			camera.position = Vector3(25.0, 25.0, -25.0)
-			camera.look_at(Vector3.ZERO)
-		2:
-			ballroom.set_wall_visibility("northwest")
-			camera.position = Vector3(-25.0, 25.0, -25.0)
-			camera.look_at(Vector3.ZERO)
-		3:
-			ballroom.set_wall_visibility("southwest")
-			camera.position = Vector3(-25.0, 25.0, 25.0)
-			camera.look_at(Vector3.ZERO)
-	print(view_point, camera.position)
+func _process(delta: float) -> void:
+	ballroom.set_wall_visibility(camera_pivot.rotation.y)
+
+func rotate_camera() -> void:
+	if rotation_tween:
+		rotation_tween.kill()
+	rotation_tween = create_tween()
+	var target_rotation = Vector3(0.0, current_rotation_target, 0.0)
+	rotation_tween.tween_property(camera_pivot, "rotation", target_rotation, 1.0)
+	
